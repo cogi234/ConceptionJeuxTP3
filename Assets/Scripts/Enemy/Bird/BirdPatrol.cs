02b86d43fbe0f39ee77b9429ebc020b4597e27d1
cogi234 <colinbougie@gmail.com>
2023-11-20 19:26:51 -0500

    Update BirdPatrol.cs

--- Assets/Scripts/Enemy/Bird/BirdPatrol.cs
+++ Assets/Scripts/Enemy/Bird/BirdPatrol.cs
@@ -24,15 +24,15 @@ public class BirdPatrol : MonoBehaviour
     private void SetupTree()
     {
         l1 = new Detect(gameObject.transform);
-         
+
         Node l2 = new Return(gameObject, Speed);
-        Node Invert1 = new Inverter(new List<Node> {l2});
+        Node Invert1 = new Inverter(new List<Node> { l2 });
         Node Seq1 = new Sequence(new List<Node> { Invert1, l1 });
         Node l3 = new AlertPhase(scream);
-        Node l4 = new Pursuit(player,gameObject.transform,Speed);
-        Node Seq2 = new Sequence(new List<Node> { Seq1, l3,l4 });
-        Node l5 = new Patrol(gameObject.transform,waypoints,Speed);
-        Node sel1 = new Selector(new List<Node>() { Seq2, l2,l5 });
+        Node l4 = new Pursuit(player, gameObject.transform, Speed);
+        Node Seq2 = new Sequence(new List<Node> { Seq1, l3, l4 });
+        Node l5 = new Patrol(gameObject.transform, waypoints, Speed);
+        Node sel1 = new Selector(new List<Node>() { Seq2, l2, l5 });
 
         root = sel1;
     }
